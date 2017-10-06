578c28405bfe7a3870f61b461dae026d5ae39646
SauravKanchan <sauravnk30@gmail.com>
2017-10-06 21:05:52 +0530

    Added charts

--- population/views.py
+++ population/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def home(requests):
-    if requests.method == "POST":
-        print(requests.post.get('img'))
+
+
     return render(requests,'population.html',{})
 
