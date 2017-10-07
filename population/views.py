2b52fd5dfe36388ea51fe8d85b57d5ac799d1359
SauravKanchan <sauravnk30@gmail.com>
2017-10-07 09:54:36 +0530

    Added bar graph and made it dynamic

--- population/views.py
+++ population/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 
 def home(requests):
+    context = {}
+    context['current_data'] = [1,5,6]
+
+    #ML
 
 
-    return render(requests,'population.html',{})
+    context['predicted_data'] = [2,12,8]
+    return render(requests,'population.html',context)
 
